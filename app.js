ee9b4a523dd4183e6f3b0182f25b559bf57b8b6a
sheltonanton <sheltonanton@gmail.com>
2022-01-04 22:08:21 +0530

    removed unused file app.js; added check for allowing test related presets during jest test

--- app.js
+++
@@ -1,1 +0,0 @@
-import Editor from '../components/editor';
--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,25 @@
-module.exports = {
-    presets: ['@babel/preset-env'],
-    plugins: [
+module.exports = (options) => {
+  let config = {
+    presets: ['@babel/preset-env']
+  }
+
+  if(options.env("test")) {
+    config = Object.assign(config, {
+      plugins: [
         [
-            "@babel/plugin-transform-runtime",
-            {
-                "regenerator": true
-            }
+          "@babel/plugin-transform-runtime",
+          {
+            "regenerator": true
+          }
         ], [
-            "babel-plugin-transform-require-ignore",
-            {
-                "extensions": [".css"]
-            }
+          "babel-plugin-transform-require-ignore",
+          {
+            "extensions": [".css"]
+          }
         ]
-    ]
+      ]
+    });
+  }
+
+  return config;
 }
